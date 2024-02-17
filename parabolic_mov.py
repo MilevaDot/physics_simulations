adc648d9cd3945cfa6fffaf915b75cae039ab63b
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2024-02-17 12:16:01 -0500

    [FEAT]: Add components values of position and velocity. Also time

--- parabolic_mov.py
+++ parabolic_mov.py
@@ -22,6 +22,7 @@ def time_generator(start, end, step):
 class Particle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # Position
         self.rect = pygame.Rect(10, HEIGHT-30, 20, 20)
         self.image = pygame.Surface((50,50), pygame.SRCALPHA)
         pygame.draw.circle(self.image, (255, 0, 0), (10, 10), 10)
@@ -123,6 +124,8 @@ class VelocityY(pygame.sprite.Sprite):
 
 pygame.init()
 
+font = pygame.font.Font(None, 36)
+text_color = (255, 255, 255)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Parabolic movement')
 clock = pygame.time.Clock()
@@ -150,6 +153,28 @@ while running:
     sprites.draw(screen)
     pygame.draw.line(screen, 'green', (10, HEIGHT-10),(WIDTH, HEIGHT-10),2)
     pygame.draw.line(screen, 'green', (10, HEIGHT-10), (10, 0), 2)
+
+    position_x_text = 'Position x: %s' % ( particle.rect.x - 10 )
+    position_y_text = 'Position y: %s' % ( 690 - particle.rect.y)
+
+    text_surface = font.render(position_x_text, True, text_color)
+    screen.blit(text_surface, (20, 10))
+    text_surface = font.render(position_y_text, True, text_color)
+    screen.blit(text_surface, (20, 50))
+
+    velocity_x_text = 'Velocity x: %s' % ( round( velocity_x.initial_velocity * math.cos(math.radians(velocity_x.initial_angle)), 4 ) )
+    velocity_y_text = 'Velocity y: %s' % ( round( velocity_y.initial_velocity * math.sin(math.radians(velocity_y.initial_angle)) - velocity_y.gravity * velocity_y.time_elapsed, 4 ) )
+
+    text_surface = font.render(velocity_x_text, True, text_color)
+    screen.blit(text_surface, (210, 10))
+    text_surface = font.render(velocity_y_text, True, text_color)
+    screen.blit(text_surface, (210, 50))
+
+    time_text = 'Time: %s' % ( round( particle.time_elapsed, 2 ) )
+
+    text_surface = font.render(time_text, True, text_color)
+    screen.blit(text_surface, (450, 10))
+
     screen.blit(velocity_x.image, velocity_x.rect.topleft)
     screen.blit(velocity_y.image, velocity_y.rect.topleft)
     pygame.display.flip()
